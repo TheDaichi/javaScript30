4fb2eb1467e6e7e9341fb022851c4e1f7243a41c
TheDaichi <neu_tron@yahoo.com>
2020-10-04 13:39:56 +0530

    :fire: add auto tune drum

--- JavaScript Drum Kit/script.js
+++ JavaScript Drum Kit/script.js
@@ -1,5 +1,7 @@
 const keys_ = Array.from(document.querySelectorAll('.key[data-key]'));
 const sounds = Array.from(document.querySelectorAll('audio'));
+let playing = false;
+let timeInterval = [];
 
 keys_.forEach((key) =>
   key.addEventListener('transitionend', function (e) {
@@ -13,7 +15,8 @@ async function handleKey(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
-  await audio.play();
+  var playPromise = audio.play();
+  playPromise.catch((e) => console.log('Checking'));
   key.classList.add('playing');
   // const keyPressed = keys_.find((k) => parseInt(k.dataset.key) === e.keyCode);
   // const soundUsed = sounds.find(
@@ -27,3 +30,24 @@ async function handleKey(e) {
   // }
 }
 window.addEventListener('keydown', handleKey);
+
+window.onload = function () {
+  const button = document.createElement('button');
+  button.innerText = 'Play Auto Music';
+  button.classList.add('playButton');
+  document.body.append(button);
+  const b = document.querySelector('.playButton');
+  b.addEventListener('click', (e) => {
+    playing = true;
+    timeInterval.push(
+      setInterval(() => {
+        if (playing) {
+          console.log(playing);
+          const a = [65, 83, 68, 70, 71, 72, 74, 75, 76];
+          const keyCode = a[Math.floor(Math.random() * a.length)];
+          handleKey({ keyCode });
+        }
+      }, 550)
+    );
+  });
+};
